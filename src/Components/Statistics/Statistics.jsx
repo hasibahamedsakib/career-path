832716e4345eaea1bac8a28e31639f645b7af82c
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-04-11 20:11:16 +0600

    recharts added

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Statistics = () => {
+  const data = [
+    {
+      name: "Assignment 1",
+      mark: 60,
+    },
+    {
+      name: "Assignment 2",
+      mark: 40,
+    },
+    {
+      name: "Assignment 3",
+      mark: 60,
+    },
+    {
+      name: "Assignment 4",
+      mark: 50,
+    },
+    {
+      name: "Assignment 5",
+      mark: 55,
+    },
+    {
+      name: "Assignment 6",
+      mark: 38,
+    },
+    {
+      name: "Assignment 7",
+      mark: 60,
+    },
+  ];
   return (
-    <div>
-      <h1>Statistics page</h1>
+    <div className="container mx-auto">
+      <AreaChart width={1020} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="mark" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
     </div>
   );
 };
