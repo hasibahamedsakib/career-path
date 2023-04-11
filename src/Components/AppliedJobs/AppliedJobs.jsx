d8518717eb661d5b405cc2966ff274629307b148
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-04-11 22:57:05 +0600

    get select value

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -4,6 +4,8 @@ import ShowJobs from "./ShowJobs";
 
 const AppliedJobs = () => {
   const [totalJobs, setTotalJobs] = useState([]);
+  const [select, setSelect] = useState("");
+  console.log(select);
 
   useEffect(() => {
     fetch("featuredJobs.json")
@@ -18,24 +20,22 @@ const AppliedJobs = () => {
       allJobs.push(jobs);
     }
   }
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    console.log("click");
-  };
+
   // console.log(allJobs);
   return (
     <div>
-      <h1 className="text-center text-4xl bg-[#f9f9ff] p-14  font-bold">
+      <h1 className="text-center text-4xl bg-[#f9f9ff] p-14  font-bold ">
         Applied Jobs
       </h1>
       <div className="container">
         <div className="text-right">
-          <select className="border px-5 py-2 bg-slate-100 outline-none">
-            <option onClick={() => handleChange()}>Filter By </option>
-            <option onChange={() => handleChange()} value="remote">
-              Remote
-            </option>
-            <option value="onsite">Onsite</option>
+          <select
+            onChange={(e) => setSelect(e.target.value)}
+            className="border px-5 py-2 bg-slate-100 outline-none"
+          >
+            <option>Filter By </option>
+            <option>Remote</option>
+            <option>Onsite</option>
           </select>
         </div>
         {allJobs &&
